8623229bf86c5609ab1550996ce23b8e46a40350
Peyman Amiri <93652974+PeymanAmirii@users.noreply.github.com>
2021-12-14 04:49:54 -0500

    Update test_My_module.py

--- testing/test_My_module.py
+++ testing/test_My_module.py
@@ -1,1 +1,6 @@
+from Module_folder.My_module import adding #from folder.module import function
 
+def test_adding_1():  # should start with test
+	assert adding(2,3) == 5
+def test_adding_2():  # should start with test
+	assert adding(2,4) == 6
